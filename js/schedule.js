28344af0ed64b3541156c695dd3d5fa867c1d54b
luca <lscarci17@gmail.com>
2017-11-13 00:39:35 -0500

    Functioning Schedule
    Added data to schedule and functioning date checker

--- js/schedule.js
+++ js/schedule.js
@@ -1,58 +1,192 @@
-var data= [
+
+var data = [
   {
-    "Sessions": "javascript: swoop em with the oop",
-    "Instructors": "Kaspar So & Hubert Si",
-    "Room": 220,
-    "Starts": "1:00PM",
-    "Ends": "4:00PM",
-    "Day": "Tuesdays",
-    "Description": "Learn the basics of Javascript"
+    "session": "javascript: swoop em with the oop",
+    "instructors": "Kaspar So & Hubert Si",
+    "room": 220,
+    "starts": "1:00",
+    "ends": "4:00",
+    "day": "Tuesdays",
+    "description": "Learn the basics of Javascript"
   },
   {
-    "Sessions": "Markup & Styling",
-    "Instructors": "Emma Wootton & JJ Bastida",
-    "Room": 218,
-    "Starts": "3:00PM",
-    "Ends": "5:00PM",
-    "Day": "Tuesdays",
-    "Description": "Learn the fundamentals of HTML/CSS"
+    "session": "Markup & Styling",
+    "instructors": "Emma Wootton & JJ Bastida",
+    "room": 218,
+    "starts": "3:00",
+    "ends": "5:00",
+    "day": "Tuesdays",
+    "description": "Learn the fundamentals of HTML/CSS"
   },
   {
-    "Sessions": "Mentorship Night",
-    "Instructors": "Carol Cheung & Senior IXD Mentors",
-    "Room": 208,
-    "Starts": "12:00PM",
-    "Ends": "5:00PM",
-    "Day": "Wednesdays",
-    "Description": "Receive aid in 3D Design & Conceptual Process"
+    "session": "Mentorship Night",
+    "instructors": "Carol Cheung & Senior IXD Mentors",
+    "room": 208,
+    "starts": "12:00",
+    "ends": "5:00",
+    "day": "Wednesdays",
+    "description": "Receive aid in 3D Design & Conceptual Process"
   },
   {
-    "Sessions": "Three to the Jay to the S",
-    "Instructors": "Scott Wilson",
-    "Room": 220,
-    "Starts": "3:00PM",
-    "Ends": "5:00PM",
-    "Day": "Thursdays",
-    "Description": "Understand how to use the three.js library"
+    "session": "Three to the Jay to the S",
+    "instructors": "Scott Wilson",
+    "room": 220,
+    "starts": "3:00",
+    "ends": "5:00",
+    "day": "Thursdays",
+    "description": "Understand how to use the three.js library"
   },
   {
-    "Sessions": "Product: UX/UI",
-    "Instructors": "Harmoney Lee & Emma Wootton",
-    "Room": 220,
-    "Starts": "5:30PM",
-    "Ends": "7:30PM",
-    "Day": "Thursdays",
-    "Description": "Going over Research Methods and Design & Systems Thinking"
+    "session": "Product: UX/UI",
+    "instructors": "Harmoney Lee & Emma Wootton",
+    "room": 220,
+    "starts": "5:30",
+    "ends": "7:30",
+    "day": "Thursdays",
+    "description": "Going over Research Methods and Design & Systems Thinking"
   },
   {
-    "Sessions": "Portoflio Chit Chat",
-    "Instructors": "Josh Millard",
-    "Room": 208,
-    "Starts": "2:00PM",
-    "Ends": "5:00PM",
-    "Day": "Fridays",
-    "Description": "Portfolio Do's and Don'ts"
+    "session": "Portoflio Chit Chat",
+    "instructors": "Josh Millard",
+    "room": 208,
+    "starts": "2:00",
+    "ends": "5:00",
+    "day": "Fridays",
+    "description": "Portfolio Do's and Don'ts"
   }
 ]
 
-console.log(data);
+
+
+
+
+var sd = document.getElementById("s-day"); //all upcoming sessions
+var st = document.getElementById("s-time"); //all upcoming sessions
+var stl = document.getElementById("s-title"); //all upcoming sessions
+var si = document.getElementById("s-instructors"); //all upcoming sessions
+var sr = document.getElementById("s-room"); //all upcoming sessions
+
+
+var cwd;
+
+var currentDay;
+
+function weekdays() {
+
+    var d = new Date();
+    var wd = new Array(7);
+    wd[0] = "Sunday";
+    wd[1] = "Monday";
+    wd[2] = "Tuesday";
+    wd[3] = "Wednesday";
+    wd[4] = "Thursday";
+    wd[5] = "Friday";
+    wd[6] = "Saturday";
+
+    var cwd = wd[d.getDay()];
+	
+
+
+		if (cwd == "Sunday") {
+				console.log("Sunday");
+				current();
+			
+			} else if (cwd =="Monday") {
+				console.log("Monday");
+				current();
+
+			} else if (cwd =="Tuesday") {
+				console.log("Tuesday");
+				current();
+				
+			} else if (cwd =="Wednesday") {
+				console.log("Wednesday");
+				current();
+				
+			} else if (cwd =="Thursday") {
+				console.log("Thursday");
+				current();
+				
+			} else if (cwd =="Friday") {
+				console.log("Friday");
+				current();
+				
+			} else if (cwd =="Saturday") {
+				console.log("Saturday");
+				current(wd);
+				
+			} 
+	function current() {
+
+		    // sd.innerHTML = data[1].day;
+		    sd.innerHTML = "Today";
+		    st.innerHTML = data[currentDay].starts + "<span>" + "PM" + "</span>";
+		    stl.innerHTML = data[currentDay].session; 
+		    si.innerHTML = data[currentDay].instructors;
+		    sr.innerHTML = "Room J" + data[currentDay].room;
+
+		   
+	}
+}
+			
+			
+
+
+
+
+
+
+
+// for (var i = 0; i < data.length; i++) {
+
+// 	sm = document.createElement("div");
+//     	 document.body.appendChild(sm);
+
+//     	 sm.id = "session-" + i;
+
+// }
+
+// s
+
+// var s_out = [];
+
+// var d = data[0].session
+
+
+// for (var i = 0; i < data.length; i++) {
+//   // s_out = data[i].push(number);
+//   s_out = d.push(i);
+
+
+// }
+
+
+// console.log(s_out);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
