b589ba6ce2328187015afd25a3ed21530ca0978c
luca <lscarci17@gmail.com>
2017-11-12 19:26:57 -0500

    Initial upload 1.0
    Upload all files to gi

---
+++ js/schedule.js
@@ -0,0 +1,58 @@
+var data= [
+  {
+    "Sessions": "javascript: swoop em with the oop",
+    "Instructors": "Kaspar So & Hubert Si",
+    "Room": 220,
+    "Starts": "1:00PM",
+    "Ends": "4:00PM",
+    "Day": "Tuesdays",
+    "Description": "Learn the basics of Javascript"
+  },
+  {
+    "Sessions": "Markup & Styling",
+    "Instructors": "Emma Wootton & JJ Bastida",
+    "Room": 218,
+    "Starts": "3:00PM",
+    "Ends": "5:00PM",
+    "Day": "Tuesdays",
+    "Description": "Learn the fundamentals of HTML/CSS"
+  },
+  {
+    "Sessions": "Mentorship Night",
+    "Instructors": "Carol Cheung & Senior IXD Mentors",
+    "Room": 208,
+    "Starts": "12:00PM",
+    "Ends": "5:00PM",
+    "Day": "Wednesdays",
+    "Description": "Receive aid in 3D Design & Conceptual Process"
+  },
+  {
+    "Sessions": "Three to the Jay to the S",
+    "Instructors": "Scott Wilson",
+    "Room": 220,
+    "Starts": "3:00PM",
+    "Ends": "5:00PM",
+    "Day": "Thursdays",
+    "Description": "Understand how to use the three.js library"
+  },
+  {
+    "Sessions": "Product: UX/UI",
+    "Instructors": "Harmoney Lee & Emma Wootton",
+    "Room": 220,
+    "Starts": "5:30PM",
+    "Ends": "7:30PM",
+    "Day": "Thursdays",
+    "Description": "Going over Research Methods and Design & Systems Thinking"
+  },
+  {
+    "Sessions": "Portoflio Chit Chat",
+    "Instructors": "Josh Millard",
+    "Room": 208,
+    "Starts": "2:00PM",
+    "Ends": "5:00PM",
+    "Day": "Fridays",
+    "Description": "Portfolio Do's and Don'ts"
+  }
+]
+
+console.log(data);
